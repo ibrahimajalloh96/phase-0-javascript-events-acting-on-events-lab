f0f4ffe3a70f0e0b221017be95781db0c71aa963
Ibrahima <ibrahimajalloh@gmail.com>
2023-10-25 16:14:27 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left) || 0;
+    const newLeft = currentLeft - 10;
+    
+    if (newLeft >= 0) {
+      dodger.style.left = newLeft + 'px';
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left) || 0;
+    const newLeft = currentLeft + 10;
+    const containerWidth = 400; // Set this to the width of your container
+    
+    if (newLeft <= containerWidth - 40) {
+      dodger.style.left = newLeft + 'px';
+    }
+  }
+  
+  // Add an event listener for left arrow key (key code 37)
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    }
+  });
+  
+  // Add an event listener for right arrow key (key code 39)
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
